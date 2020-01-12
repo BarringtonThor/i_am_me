1746760e9a3f054207b350d1e652debeb9051537
Barrington <Barringtonthorpe@gmail.com>
2020-01-11 18:06:27 -0600

    Imported Picture

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello Barrington'),
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(19, 19, 19, 1),
+        appBar: AppBar(
+          title: Text('I\'m Me'),
+          backgroundColor: Color.fromRGBO(29, 29, 29, 1),
+          centerTitle: true,
+          textTheme: TextTheme(
+            title: TextStyle(
+              color: Color.fromRGBO(187, 122, 245, 1),
+              fontSize: 30.0,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('Images/IMG_0471.jpg'),
+          ),
+        ),
       ),
     ),
   );
