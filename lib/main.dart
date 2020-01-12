86a529c6ca8f783c4ed3d2d5f7255e7b3e678da4
Barrington <Barringtonthorpe@gmail.com>
2020-01-11 22:22:04 -0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromRGBO(19, 19, 19, 1),
         appBar: AppBar(
@@ -17,11 +22,25 @@ void main() {
           ),
         ),
         body: Center(
-          child: Image(
-            image: AssetImage('Images/IMG_0471.jpg'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 175.0,
+                backgroundImage: AssetImage('Images/IMG_0471.jpg'),
+              ),
+              Text(
+                'Barrington Thorpe',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Color(0xff03DAC5),
+                ),
+              ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
